a7fd6b2ccb4c631efe0621bb8217a8e8dc2521bc
Yaroslav Shirokov <slava@github.com>
2013-05-24 23:03:53 -0700

    Scaffolding polygon

--- src/csgtool.c
+++ src/csgtool.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-
+#include "klist.h"
 #include "dbg.h"
-
 #include "stl.h"
+
+#define mp_float3_free(x) free(kl_val(x))
+KLIST_INIT(float3, float3*, mp_float3_free)
+
+typedef struct s_poly {
+	klist_t(float3) *vertices;
+	float3 normal;
+	float w;
+} poly_t;
+
+poly_t *alloc_poly(void) {
+	poly_t *poly = calloc(1, sizeof(poly_t));
+	check_mem(poly);
+	poly->vertices = kl_init(float3);
+	return poly;
+error:
+	return NULL;
+}
+
+void free_poly(poly_t *p) {
+	kl_destroy(float3, p->vertices);
+}
 
 int main(int argc, char **argv) {
 	check(argc >= 2, "Need a filename");
