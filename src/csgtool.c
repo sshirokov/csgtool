d8d9fc3c044f118372de9a1ea40994cb3eef5020
Yaroslav Shirokov <slava@github.com>
2013-05-25 00:32:37 -0700

    WIP: poly_update() normal is computed

--- src/csgtool.c
+++ src/csgtool.c
@@ -28,11 +28,59 @@ void free_poly(poly_t *p) {
 #define mp_poly_free(x) free(kl_val(x))
 KLIST_INIT(poly, poly_t*, mp_poly_free)
 
+// TODO: move
+float3 *f3_cross(float3 *result, float3 v1, float3 v2) {
+		float3 v1_x_v2 = {
+				v1[1]*v2[2] - v1[2]*v2[1],
+				v1[2]*v2[0] - v1[0]*v2[2],
+				v1[0]*v2[1] - v1[1]*v2[0]
+		};
+		memcpy(result, &v1_x_v2, sizeof(float3));
+		return result;
+}
+
+float3 *f3_normalize(float3 *v) {
+	float mag = sqrt((*v)[0] * (*v)[0] +
+					 (*v)[1] * (*v)[1] +
+					 (*v)[2] * (*v)[2]);
+	(*v)[0] /= mag;
+	(*v)[1] /= mag;
+	(*v)[2] /= mag;
+	return v;
+}
+
+float3 *f3_sub(float3 *result, float3 v1, float3 v2) {
+	float3 r = {v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]};
+	memcpy(result, r, sizeof(float3));
+	return result;
+}
+
+int poly_update(poly_t *poly) {
+	check(poly->vertices->size > 2,
+		  "poly_update(Polyon(%p)): has %zd verticies.",
+		  poly, poly->vertices->size);
+
+	kliter_t(float3) *v_iter = kl_begin(poly->vertices);
+	float3 *a = kl_val(v_iter);
+	float3 *b = kl_val(v_iter = kl_next(v_iter));
+	float3 *c = kl_val(v_iter = kl_next(v_iter));
+
+	float3 b_a;
+	float3 c_a;
+	f3_sub(&b_a, *b, *a);
+	f3_sub(&c_a, *c, *a);
+
+	f3_cross(&poly->normal, b_a, c_a);
+	f3_normalize(&poly->normal);
+	return 0;
+error:
+	return -1;
+}
 
 int main(int argc, char **argv) {
 	check(argc >= 2, "Need a filename");
 	char *file = argv[1];
-	stl_object *file_stl = stl_read_file(file);
+	stl_object *file_stl = stl_read_file(file, 0);
 	check(file_stl != NULL, "Failed to read stl from '%s'", file);
 	log_info("Loaded file: %s %d facets", file, file_stl->facet_count);
 
@@ -52,7 +100,7 @@ int main(int argc, char **argv) {
 			memcpy(f, face->vertices[v], sizeof(float3));
 			*kl_pushp(float3, poly->vertices) = f;
 		}
-
+		poly_update(poly);
 
 		printf("Adding %d/%d\r", i+1, file_stl->facet_count);
 	}
