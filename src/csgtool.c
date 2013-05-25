1791ae8f13c9a65dd0cfc068743103674b768c1f
Yaroslav Shirokov <slava@github.com>
2013-05-24 23:33:21 -0700

    WIP: Packing polygons

--- src/csgtool.c
+++ src/csgtool.c
@@ -25,18 +25,46 @@ void free_poly(poly_t *p) {
 	kl_destroy(float3, p->vertices);
 }
 
+#define mp_poly_free(x) free(kl_val(x))
+KLIST_INIT(poly, poly_t*, mp_poly_free)
+
+
 int main(int argc, char **argv) {
 	check(argc >= 2, "Need a filename");
 	char *file = argv[1];
 	stl_object *file_stl = stl_read_file(file);
 	check(file_stl != NULL, "Failed to read stl from '%s'", file);
-
 	log_info("Loaded file: %s %d facets", file, file_stl->facet_count);
 
+	klist_t(poly) *polygons = kl_init(poly);
+	for(int i = 0; i < file_stl->facet_count; i++) {
+		stl_facet *face = &file_stl->facets[i];
+		poly_t *poly = NULL;
 
+		check(poly = alloc_poly(), "Failed to allocate polygon %d", i);
+		*kl_pushp(poly, polygons) = poly;
+
+		// Copy each vertex, using a fresh pointer
+		// and letting the poly deallocator deal with it
+		float3 *f = NULL;
+		for(int v = 0; v < 3; v++) {
+			check_mem(f = malloc(sizeof(float3)));
+			memcpy(f, face->vertices[v], sizeof(float3));
+			*kl_pushp(float3, poly->vertices) = f;
+		}
+
+
+		printf("Adding %d/%d\r", i+1, file_stl->facet_count);
+	}
+	putchar('\n');
+
+	kl_destroy(poly, polygons);
 	stl_free(file_stl);
+	log_info("Terminating Success");
 	return 0;
 error:
+	kl_destroy(poly, polygons);
 	if(file_stl != NULL) stl_free(file_stl);
+	log_err("Terminating Failure");
 	return -1;
 }
