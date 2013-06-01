af4c9239edd81834154148fdd784dfe056bf78c5
Yaroslav Shirokov <slava@github.com>
2013-06-01 01:43:38 -0700

    Cleaned up the bsp test to use poly_make_triangle()

--- tests/bsp.c
+++ tests/bsp.c
@@ -7,67 +7,29 @@ klist_t(poly) *polygons;
 
 void test_bsp__initialize(void) {
 	bsp = alloc_bsp_node();
-	cl_assert_(bsp != NULL, "Out of memory");
-
 	poly_t *poly = NULL;
-	poly = alloc_poly();
-	cl_assert_(poly != NULL, "Out of memory");
-
 	polygons = kl_init(poly);
 
-	float3 *f = NULL;
-
-	// 1st
-	f = calloc(1, sizeof(float3));
-	(*f)[0] = -0.5;
-	(*f)[1] = 0.0;
-	(*f)[2] = 0.0;
-	*kl_pushp(float3, poly->vertices) = f;
-
-	f = calloc(1, sizeof(float3));
-	(*f)[0] = 0.5;
-	(*f)[1] = 0.0;
-	(*f)[2] = 0.0;
-	*kl_pushp(float3, poly->vertices) = f;
-
-	f = calloc(1, sizeof(float3));
-	(*f)[0] = 0.0;
-	(*f)[1] = 0.5;
-	(*f)[2] = 0.0;
-	*kl_pushp(float3, poly->vertices) = f;
-
-	poly_update(poly);
+	float3 fs[] = {{-0.5, 0.0, 0.0},
+				   {0.5, 0.0, 0.0},
+				   {0.0, 0.5, 0.0}};
+	poly = poly_make_triangle(fs[0], fs[1], fs[2]);
+	cl_assert_(poly != NULL, "Out of memory");
 	*kl_pushp(poly, polygons) = poly;
 
-	poly = alloc_poly();
-
-	// 2nd
-	f = calloc(1, sizeof(float3));
-	(*f)[0] = -0.5;
-	(*f)[1] = 0.0;
-	(*f)[2] = -0.5;
-	*kl_pushp(float3, poly->vertices) = f;
-
-	f = calloc(1, sizeof(float3));
-	(*f)[0] = 0.5;
-	(*f)[1] = 0.0;
-	(*f)[2] = -0.5;
-	*kl_pushp(float3, poly->vertices) = f;
-
-	f = calloc(1, sizeof(float3));
-	(*f)[0] = 0.0;
-	(*f)[1] = 0.5;
-	(*f)[2] = -0.5;
-	*kl_pushp(float3, poly->vertices) = f;
-
-	poly_update(poly);
+	float3 fs2[] = {{-0.5, 0.0, -0.5},
+				   {0.5, 0.0, -0.5},
+				   {0.0, 0.5, -0.5}};
+	poly = poly_make_triangle(fs2[0], fs2[1], fs2[2]);
+	cl_assert_(poly != NULL, "Out of memory");
 	*kl_pushp(poly, polygons) = poly;
 
+	cl_assert_(bsp != NULL, "Out of memory");
 	cl_assert_(bsp_build(bsp, polygons) != NULL, "Failed to build bsp tree");
 }
 
 void test_bsp__cleanup(void) {
-
+	// TODO: free_bsp() is a segfault, let's not do that at all
 }
 
 void test_bsp__root_has_poly(void) {
