efb9f9d6a9a960e4a12b40a80ef0cbc4ddf7f26f
Yaroslav Shirokov <slava@github.com>
2013-06-01 20:28:51 -0700

    bsp_to_stl() exporter

--- src/export.c
+++ src/export.c
@@ -23,3 +23,23 @@ error:
 	if(stl) stl_free(stl);
 	return NULL;
 }
+
+stl_object *bsp_to_stl(bsp_node_t *tree) {
+	stl_object *stl = NULL;
+	klist_t(poly) *polys = NULL;
+
+	polys = bsp_to_polygons(tree, 1, NULL);
+	check(polys != NULL, "Failed to generate polygons from bsp_node_t(%p)", tree);
+	check(polys->size > 0, "No polygons returned from tree(%p)", tree);
+
+	stl = stl_from_polys(polys);
+	check(stl != NULL, "Failed to build stl from %zd polygons", polys->size);
+	strcpy(stl->header, "[csgtool BSP output]");
+
+	kl_destroy(poly, polys);
+	return stl;
+error:
+	if(polys) kl_destroy(poly, polys);
+	if(stl) stl_free(stl);
+	return NULL;
+}
