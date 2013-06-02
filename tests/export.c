8cc4f1ad9f4bb0b46c6683849f7576ce5693aebd
Yaroslav Shirokov <slava@github.com>
2013-06-01 19:38:25 -0700

    Generating a quad for the export test

--- tests/export.c
+++ tests/export.c
@@ -3,15 +3,31 @@
 #include "stl.h"
 #include "bsp.h"
 
-char jaws_path[] = CLAR_FIXTURE_PATH "jaws.stl";
-stl_object *jaws_object = NULL;
-bsp_node_t *jaws_bsp = NULL;
+stl_object *stl = NULL;
+bsp_node_t *bsp_tree = NULL;
 
 void test_export__initialize(void) {
+	// I'll make a quad by making a triangle with a missing
+	// vertex, then pushing the extra vertex after and recomputing
+	float3 quad_verts[] = {{-1.0, 1.0, 0.0},
+						  {-1.0, -1.0, 0.0},
+						  {1.0, -1.0, 0.0},
+						  {1.0, 1.0, 0.0}};
+	poly_t *poly = poly_make_triangle(quad_verts[0], quad_verts[1], quad_verts[2]);
+	cl_assert_(poly != NULL, "Can't make triangle for test");
 
+	float3 *f3rc = *kl_pushp(float3, poly->vertices) = clone_f3(quad_verts[3]);
+	cl_assert_(f3rc != NULL, "Failed to clone vertex into quad");
+	poly_update(poly);
+
+	// Build a tree of the quad
+	klist_t(poly) *lpoly = kl_init(poly);
+	*kl_pushp(poly, lpoly) = poly;
+	bsp_tree = bsp_build(NULL, lpoly);
+	cl_assert(bsp_tree);
 }
 
 void test_export__cleanup(void) {
-	if(jaws_object) stl_free(jaws_object);
-	// TODO: free_bsp()
+	if(stl) stl_free(stl);
+	// TODO: free_bsp(bsp)
 }
