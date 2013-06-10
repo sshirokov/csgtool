3162b50e4917db1321b56d911a2814612bfe1faa
Sean Bryant <sbryant@github.com>
2013-06-10 14:17:44 -0700

    whitespace nit.

--- src/vector.c
+++ src/vector.c
@@ -35,7 +35,7 @@ float f3_dot(float3 v1, float3 v2) {
 
 float3 *f3_cross(float3 *result, float3 v1, float3 v2) {
 		(*result)[0] = v1[1]*v2[2] - v1[2]*v2[1];
- 		(*result)[1] = v1[2]*v2[0] - v1[0]*v2[2];
+		(*result)[1] = v1[2]*v2[0] - v1[0]*v2[2];
 		(*result)[2] = v1[0]*v2[1] - v1[1]*v2[0];
 		return result;
 }
