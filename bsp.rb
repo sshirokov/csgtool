a113304e6e4dd45a659351b5671351e12c3943ce
Yaroslav Shirokov <slava@github.com>
2013-06-11 10:43:19 -0700

    Move the native stuff into a CSG::Native

--- bsp.rb
+++ bsp.rb
@@ -1,52 +1,53 @@
 require 'ffi'
 
 module CSG
-  extend FFI::Library
+  module Native
+    extend FFI::Library
+    ffi_lib 'csg'
 
-  ffi_lib 'csg'
+    FLOAT3 = FFI::ArrayType.new(FFI::Type::FLOAT, 3)
+    typedef FLOAT3, :float3
 
-  FLOAT3 = FFI::ArrayType.new(FFI::Type::FLOAT, 3)
-  typedef FLOAT3, :float3
+    class STLFacet < FFI::Struct
+      layout :normal, :float3,
+      :vertices, FFI::ArrayType.new(CSG::Native::FLOAT3, 3),
+      :attr, :uint16
+    end
 
-  class STLFacet < FFI::Struct
-    layout :normal, :float3,
-           :vertices, FFI::ArrayType.new(CSG::FLOAT3, 3),
-           :attr, :uint16
+    class STLObject < FFI::Struct
+      layout :header, [FFI::Type::UINT8, 80],
+      :facet_count, :uint32,
+      :stl_facet, :pointer
+    end
+
+    attach_function :stl_read_file, [:string, :bool], :pointer
+    attach_function :stl_write_file, [:pointer, :string], :int
+
+    attach_function :stl_to_bsp, [:pointer], :pointer
+    attach_function :bsp_to_stl, [:pointer], :pointer
+
+    attach_function :bsp_union, [:pointer, :pointer], :pointer
+    attach_function :bsp_subtract, [:pointer, :pointer], :pointer
+    attach_function :bsp_intersect, [:pointer, :pointer], :pointer
   end
-
-  class STLObject < FFI::Struct
-    layout :header, [FFI::Type::UINT8, 80],
-           :facet_count, :uint32,
-           :stl_facet, :pointer
-  end
-
-  attach_function :stl_read_file, [:string, :bool], :pointer
-  attach_function :stl_write_file, [:pointer, :string], :int
-
-  attach_function :stl_to_bsp, [:pointer], :pointer
-  attach_function :bsp_to_stl, [:pointer], :pointer
-
-  attach_function :bsp_union, [:pointer, :pointer], :pointer
-  attach_function :bsp_subtract, [:pointer, :pointer], :pointer
-  attach_function :bsp_intersect, [:pointer, :pointer], :pointer
 end
 
 
 
-object = CSG.stl_read_file(ARGV[0], true)
-object2 = CSG.stl_read_file(ARGV[1], true)
+object = CSG::Native.stl_read_file(ARGV[0], true)
+object2 = CSG::Native.stl_read_file(ARGV[1], true)
 
-object_bsp = CSG.stl_to_bsp(object)
-object2_bsp = CSG.stl_to_bsp(object2)
+object_bsp = CSG::Native.stl_to_bsp(object)
+object2_bsp = CSG::Native.stl_to_bsp(object2)
 
-intersect_bsp = CSG.bsp_intersect(object_bsp, object2_bsp);
-substract_bsp = CSG.bsp_subtract(object_bsp, object2_bsp);
-union_bsp = CSG.bsp_union(object_bsp, object2_bsp);
+intersect_bsp = CSG::Native.bsp_intersect(object_bsp, object2_bsp);
+substract_bsp = CSG::Native.bsp_subtract(object_bsp, object2_bsp);
+union_bsp = CSG::Native.bsp_union(object_bsp, object2_bsp);
 
-i = CSG.bsp_to_stl(intersect_bsp)
-s = CSG.bsp_to_stl(substract_bsp)
-u = CSG.bsp_to_stl(union_bsp)
+i = CSG::Native.bsp_to_stl(intersect_bsp)
+s = CSG::Native.bsp_to_stl(substract_bsp)
+u = CSG::Native.bsp_to_stl(union_bsp)
 
-CSG.stl_write_file(u, "union.stl")
-CSG.stl_write_file(s, "substract.stl")
-CSG.stl_write_file(i, "intersect.stl")
+CSG::Native.stl_write_file(u, "union.stl")
+CSG::Native.stl_write_file(s, "substract.stl")
+CSG::Native.stl_write_file(i, "intersect.stl")
