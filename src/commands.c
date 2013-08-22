8a990636b1d934f4d702dba2733a682568318732
Yaroslav Shirokov <slava@github.com>
2013-08-21 22:11:26 -0700

    Docs.

--- src/commands.c
+++ src/commands.c
@@ -1,6 +1,13 @@
 #include <string.h>
 #include "commands.h"
 
+// CLI Command functions
+// Each takes argc, counted after the command name
+// and argv, with argc elements with the remainder of the
+// command line, the commands return status is directly
+// returned to the OS.
+//
+// Commands should be exported in `commands` at the bottom of this file.
 int cmd_intersect(int argc, char **argv) {
 
 	return 0;
@@ -13,6 +20,7 @@ const cmd_t commands[] = {
 	{NULL, NULL, NULL}
 };
 
+// Search for a command by name.
 cmd_fun_t cmd_find(const char *name) {
 	for(cmd_t *c = (cmd_t*)commands; c->name != NULL; c++) {
 		if(0 == strcmp(c->name, name)) return c->fun;
