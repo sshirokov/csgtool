d8b8c52bbc72895fa3f1fabbbf269f12ed848d3e
Yaroslav Shirokov <slava@github.com>
2013-08-30 01:07:22 -0700

    Minor clean up of cmd_clean() so that the remapped results are used.

--- src/commands.c
+++ src/commands.c
@@ -13,8 +13,11 @@
 int cmd_clean(int argc, char **argv) {
 	char *out = "./out.cleaned.stl";
 	stl_object *stl = NULL;
+	stl_object *cleaned = NULL;
 	klist_t(poly) *polys = NULL;
+	klist_t(poly) *filtered = NULL;
 	mesh_index_t *index = NULL;
+	int rc = -1;
 	check(argc >= 1, "An input file is required.");
 	check((stl = stl_read_file(argv[0], 1)) != NULL, "Failed to read file from '%s'", argv[0]);
 	check((polys = stl_to_polys(stl)) != NULL, "Failed to get polygon list from %p (%s)", stl, argv[0]);
@@ -23,35 +26,31 @@ int cmd_clean(int argc, char **argv) {
 	if(argc > 2) out = argv[1];
 
 	// Perform a simple filter to remove polys with singularity edges
-	klist_t(poly) *filtered = filter_polys(NULL, polys, filter_test_edge_singularity);
-	log_info("Filter result %zd/%zd", polys->size, filtered->size);
-	// TODO: Don't do this, but also rewrite this whole method to not be a test driver.
-
-	stl_object *f_stl = stl_from_polys(filtered);
-	log_info("Wrtiting filterd: %d", stl_write_file(f_stl, "./out.filtered.stl"));
-	stl_free(f_stl);
+	filtered = filter_polys(NULL, polys, filter_test_edge_singularity);
+	// TODO: check()s
+	log_info("Filter result %zd => %zd polygons", polys->size, filtered->size);
 	kl_destroy(poly, polys);
 	polys = filtered;
 
+	// Create an index for the mesh
 	index = alloc_mesh_index(polys);
-	size_t verts = 0;
-	size_t edges = 0;
 	check(index != NULL, "Failed to generate index of %zd polygons from %s", polys->size, argv[0]);
-	vertex_tree_walk(index->vertex_tree, vertex_node_count, &verts);
-	edge_tree_walk(index->edge_tree, edge_node_count, &edges);
-	log_info("Polys in index: %zd Verts in index: %zd Edges in index: %zd", index->polygons->size, verts, edges);
 
-	// TODO: You are here
+	// Remove bisecting vertecies by adding polygons
 	filtered = map_polys_with_index(index, NULL, polys, map_bisect_to_triangles);
-	log_info("%zd mapped polys produced.", filtered->size);
-	f_stl = stl_from_polys(filtered);
-	log_info("Wrtiting mapped: %d", stl_write_file(f_stl, "./out.mapped.stl"));
-	stl_free(f_stl);
-	kl_destroy(poly, filtered);
+	log_info("%zd mapped polys produced with index map.", filtered->size);
+	// TODO: check()
+	free_mesh_index(index);
+	kl_destroy(poly, polys);
+	polys = filtered;
 
-	klist_t(poly) *real_polys = kl_init(poly);
+	// Reindex the new set of polygons
+	index = alloc_mesh_index(filtered);
 
 	// Walk the polygons and only keep ones that return more than two neighbors
+	log_info("Removing polygons with too few neighbors.");
+	klist_t(poly) *real_polys = kl_init(poly);
+	// TODO: check
 	kliter_t(idx_poly) *iter = kl_begin(index->polygons);
 	for(; iter != kl_end(index->polygons); iter = kl_next(iter)) {
 		idx_poly_t *poly = kl_val(iter);
@@ -63,13 +62,11 @@ int cmd_clean(int argc, char **argv) {
 		kl_destroy(idx_poly, neighbors);
 	}
 
+	// Write the result
 	log_info("Result has %zd polys, writing to %s", real_polys->size, out);
-	stl_object *cleaned = stl_from_polys(real_polys);
-	int rc = stl_write_file(cleaned, out);
+	cleaned = stl_from_polys(real_polys);
+	rc = stl_write_file(cleaned, out);
 	log_info("Write result: %d", rc);
-
-
-
 
 	if(index != NULL) free_mesh_index(index);
 	if(polys != NULL) kl_destroy(poly, polys);
