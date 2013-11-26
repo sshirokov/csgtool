1915c8cc39a3c309edf080ab9839dfb4a22b9f9b
Yaroslav Shirokov <slava@github.com>
2013-11-25 16:28:27 -0800

    Revert "Append `.stl` to path in the default writer."
    This reverts commit fcbd713fbcc25d307d92fa3c63b1393c31f220ab.

--- src/mesh.c
+++ src/mesh.c
@@ -31,27 +31,18 @@ int _default_write(void *self, char *path) {
 	int rc = -1;
 	mesh_t *mesh = (mesh_t*)self;
 	stl_object *stl = NULL;
-	klist_t(poly) *polys = NULL;
-	char *out_path = NULL;
-
-	check_mem(out_path = calloc(1, strlen(path) + strlen(".stl") + 1));
-	check((polys = mesh->to_polygons(mesh)) != NULL, "Failed to get polygons from mesh %p", mesh);
-
+	klist_t(poly) *polys = mesh->to_polygons(mesh);
+	check(polys != NULL, "Failed to get polygons from mesh %p", mesh);
 
 	// The default output format is STL
-	// The output path will always end in ".stl" when using this writer
-	rc = sprintf(out_path, "%s.stl", path);
-	check(rc != -1, "Failed to generate output path.");
 	stl = stl_from_polys(polys);
 	check(stl != NULL, "Failed to generate STL object for write.");
-	rc = stl_write_file(stl, out_path);
+	rc = stl_write_file(stl, path);
 
-	if(out_path != NULL) free(out_path);
 	if(stl != NULL) stl_free(stl);
 	if(polys != NULL) kl_destroy(poly, polys);
 	return rc;
 error:
-	if(out_path != NULL) free(out_path);
 	if(stl != NULL) stl_free(stl);
 	if(polys != NULL) kl_destroy(poly, polys);
 	return -1;
