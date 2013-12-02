d0184edff2c17b732dd6497f64daf159187a8ff9
Yaroslav Shirokov <slava@github.com>
2013-12-01 21:54:17 -0800

    polys_to_tris(dst, src)
    Copy a polygon list to a triangle list.

--- src/export.h
+++ src/export.h
@@ -14,5 +14,6 @@ stl_object *stl_from_polys(klist_t(poly) *polygons);
 stl_object *bsp_to_stl(bsp_node_t *tree);
 bsp_node_t *stl_to_bsp(stl_object *stl);
 bsp_node_t *mesh_to_bsp(mesh_t *mesh);
+klist_t(poly)* polys_to_tris(klist_t(poly)* dst, klist_t(poly)* src);
 
 #endif
