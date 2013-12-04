76b07e6dddbe6af896dc3cfa7d469b0756104ba6
Yaroslav Shirokov <slava@github.com>
2013-12-03 17:15:59 -0800

    Updating ruby wrapper and csgtool.rb
    Undecided on if I want to keep the STLObject exported for write, or not.
    
    The C-API sort-of favors C, and making a mesh would probs entangle the existing
    BSPNode with the ruby GC :(

--- csgtool.rb
+++ csgtool.rb
@@ -1,12 +1,23 @@
 require 'csg'
 
+puts "Reading input", "--"
 object = CSG::Solid.new(:file => ARGV[0])
 object2 = CSG::Solid.new(:file => ARGV[1])
 
+puts "Object: #{object.inspect}"
+puts "Object2: #{object2.inspect}"
+
+puts "Performing operations", "--"
 intersect = object.intersect(object2)
 subtract = object.subtract(object2)
 union = object.union(object2)
 
-intersect.to_stl.write_file "intersect.stl"
-subtract.to_stl.write_file "subtract.stl"
-union.to_stl.write_file "union.stl"
+puts "Intersect: #{intersect.inspect}"
+puts "Union: #{union.inspect}"
+puts "Subtract: #{subtract.inspect}"
+
+puts "TODO: Writing output", "--"
+
+# intersect.to_stl.write_file "intersect.stl"
+# subtract.to_stl.write_file "subtract.stl"
+# union.to_stl.write_file "union.stl"
--- lib/csg.rb
+++ lib/csg.rb
@@ -28,8 +28,68 @@ module CSG
     attach_function :destroy_mesh, [:pointer], :void
     attach_function :mesh_read_file, [:string], Mesh
 
-    # TODO: most of this should go away and get replaced
-    #       with the CSG::Native::Mesh based stuff
+    class BSPNode < FFI::ManagedStruct
+      layout :polygons, :pointer,
+             :divider, :pointer,
+             :front, :pointer,
+             :back,  :pointer
+
+      def self.release(ptr)
+        CSG::Native.free_bsp_tree ptr
+      end
+    end
+
+    attach_function :free_bsp_tree, [:pointer], :void
+    attach_function :mesh_to_bsp, [Mesh], BSPNode
+
+    attach_function :bsp_union, [:pointer, :pointer], :pointer
+    attach_function :bsp_subtract, [:pointer, :pointer], :pointer
+    attach_function :bsp_intersect, [:pointer, :pointer], :pointer
+  end
+end
+
+module CSG
+  class Solid
+    attr_reader :tree
+
+    def initialize(opts)
+      if opts[:file]
+        load_from_file opts[:file]
+      elsif opts[:tree]
+        @tree = opts[:tree]
+      end
+      raise ArgumentError.new "Failed to load tree with: #{opts.inspect}" unless @tree
+    end
+
+    def load_from_file(path)
+      File.stat(path) # Stat before load to raise a sane "Does not exist" error
+      mesh_ptr = CSG::Native::mesh_read_file path
+      if not mesh_ptr.null?
+        if (tree_ptr = CSG::Native.mesh_to_bsp mesh_ptr)
+          @tree = CSG::Native::BSPNode.new tree_ptr
+        else
+          raise Exception.new("Failed to convert Mesh into BSPNode")
+        end
+      else
+        raise Exception.new("Failed to produce Mesh from #{path}")
+      end
+    end
+
+    [:intersect, :subtract, :union].each do |name|
+      define_method name do |solid|
+        ptr = CSG::Native.send "bsp_#{name}", tree, solid.tree
+        tree = CSG::Native::BSPNode.new(ptr)
+        CSG::Solid.new :tree => tree
+      end
+    end
+
+  end
+end
+
+# TODO: most of this should go away and get replaced
+#       with the CSG::Native::Mesh based stuff
+module CSG
+  module Native
     class STLObject < FFI::ManagedStruct
       layout :header, [:uint8, 80],
              :facet_count, :uint32,
@@ -44,32 +104,18 @@ module CSG
       end
     end
 
-    class BSPNode < FFI::ManagedStruct
-      layout :polygons, :pointer,
-             :divider, :pointer,
-             :front, :pointer,
-             :back,  :pointer
 
-      def self.release(ptr)
-        CSG::Native.free_bsp_tree ptr
-      end
-    end
 
     attach_function :stl_read_file, [:string, :bool], :pointer
     attach_function :stl_write_file, [:pointer, :string], :int
 
     attach_function :stl_free, [:pointer], :void
-    attach_function :free_bsp_tree, [:pointer], :void
 
     attach_function :stl_to_bsp, [:pointer], :pointer
     attach_function :bsp_to_stl, [:pointer], :pointer
-
-    attach_function :bsp_union, [:pointer, :pointer], :pointer
-    attach_function :bsp_subtract, [:pointer, :pointer], :pointer
-    attach_function :bsp_intersect, [:pointer, :pointer], :pointer
   end
 
-  class Solid
+  class OldSolid
     attr_reader :tree
 
     def initialize(opts)
