4a812c1f1853c7cdafe459ee5046667192f9a5bb
Yaroslav Shirokov <slava@github.com>
2014-03-25 17:54:15 -0700

    Failing test scaffold

---
+++ tests/utils.c
@@ -0,0 +1,15 @@
+#include "clar.h"
+
+#include "util.h"
+
+void test_utils__initialize(void) {
+
+}
+
+void test_utils__cleanup(void) {
+
+}
+
+void test_utils__read_line_can_read_a_line(void) {
+	cl_assert_(false, "TODO: Test the line reader");
+}
