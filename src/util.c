2673f6909b9d27b33e60f8bf88eecf5e16a69be0
Yaroslav Shirokov <slava@github.com>
2014-03-27 23:43:38 -0700

    Add str_trim

--- src/util.c
+++ src/util.c
@@ -2,6 +2,76 @@
 
 #include "util.h"
 
+char *str_dup(char *str) {
+	char *copy_str = NULL;
+	check_mem(copy_str = calloc(strlen(str) + 1, sizeof(char)));
+	strncpy(copy_str, str, strlen(str));
+	return copy_str;
+error:
+	return NULL;
+}
+
+char *str_ltrim(char *str, bool copy) {
+	if(!copy) {
+		// Find the first non-space char in the string
+		char *start = str;
+		while(*start && isspace(*start)) {
+			start++;
+		}
+
+		// Do nothing if the string starts on a sane char
+		if(start != str) {
+			// Shift the string and NULL-cap it if we found a subset
+			// otherwise NULL the head of the string
+			if((start - str) != strlen(str)) {
+				memmove(str, start, strlen(start));
+				str[strlen(start)] = '\0';
+			}
+			else {
+				str[0] = '\0';
+			}
+		}
+		return str;
+	}
+	else {
+		char *copy_str = NULL;
+		check_mem(copy_str = str_dup(str));
+		return str_ltrim(copy_str, false);
+	}
+error:
+	return NULL;
+}
+
+char *str_rtrim(char *str, bool copy) {
+	if(!copy) {
+		char *end = str + strlen(str) - 1;
+		while((end > str) && isspace(*end)) {
+			*end-- = '\0';
+		}
+		return str;
+	}
+	else {
+		char *copy_str = NULL;
+		check_mem(copy_str = str_dup(str));
+		return str_ltrim(copy_str, false);
+	}
+error:
+	return NULL;
+}
+
+char *str_trim(char *str, bool copy) {
+	char *trim_str = copy ? str_dup(str) : str;
+	check_mem(trim_str);
+
+	// Since we have already made a copy if we need one,
+	// we can just chain these two together, since non-copy
+	// operations can't throw out a NULL and the pointer itself
+	// cannot change.
+	return str_ltrim(str_rtrim(trim_str, false), false);
+error:
+	return NULL;
+}
+
 char *read_line(FILE *f, bool downcase, bool trim) {
 	char read_buffer[512] = {0};
 	char *line = NULL;
@@ -42,7 +112,7 @@ char *read_line(FILE *f, bool downcase, bool trim) {
 	// Post processing
 	if((line != NULL) && (strlen(line) > 0)) {
 		if(trim == true) {
-			log_err("TODO: trim not done");
+			line = str_trim(line, false);
 		}
 
 		if(downcase == true) {
--- src/util.h
+++ src/util.h
@@ -7,6 +7,13 @@
 #ifndef __UTIL_H
 #define __UTIL_H
 
+
+char *str_dup(char *str);
+
+char *str_ltrim(char *str, bool copy);
+char *str_rtrim(char *str, bool copy);
+char *str_trim(char *str, bool copy);
+
 /*
  * Allocate, read, and return a '\0' terminated string from `fd'.
  * Return NULL in caes of error
