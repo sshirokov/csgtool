359549eb5416bd60ba43e474a5d735ff3d24aced
Yaroslav Shirokov <slava@github.com>
2014-03-31 15:12:25 -0700

    Tests for a bunch of things

--- tests/utils.c
+++ tests/utils.c
@@ -19,11 +19,11 @@ void test_utils__cleanup(void) {
 // TODO: Test fixtures, rooted at CLAR_FIXTURE_PATH
 // each one should be their own test, p-much
 //
-// line_with_nl
-// line_without_nl
-// multiline_text
+// [x] line_with_nl
+// [x] empty
+// [x] line_without_nl
+// [x] multiline_text
 // multiline_text_no_nl
-// empty
 
 void test_utils__read_line_can_read_a_line(void) {
 	fp = fopen(CLAR_FIXTURE_PATH "line_with_nl", "r");
@@ -66,4 +66,57 @@ void test_utils__read_line_can_read_a_line_then_trim_and_downcase(void) {
 
 	// Cleanup locals
 	if(data != NULL) free(data);
+}
+
+void test_utils__read_line_doesnt_vomit_on_empty_file(void) {
+	fp = fopen(CLAR_FIXTURE_PATH "empty", "r");
+
+	char *data = read_line(fp, true, true);
+	cl_assert_(data == NULL, "Should have read NULL from an empty file.");
+
+	// Clean up anything we shouldn't have
+	if(data != NULL) free(data);
+}
+
+void test_utils__read_line_reads_without_nl(void) {
+	fp = fopen(CLAR_FIXTURE_PATH "line_without_nl", "r");
+
+	char *data = read_line(fp, false, true);
+	cl_assert_equal_i(strcmp("I am a cat", data), 0);
+
+	if(data != NULL) free(data);
+}
+
+void test_utils__read_line_by_line_from_multiline_with_nl(void) {
+	fp = fopen(CLAR_FIXTURE_PATH "multiline_text", "r");
+
+	char *data = NULL;
+
+	data = read_line(fp, false, true);
+	cl_assert_equal_i(strcmp("I am a cat", data), 0);
+	if(data != NULL) free(data);
+
+	data = read_line(fp, true, true);
+	cl_assert_equal_i(strcmp("we are all cats", data), 0);
+	if(data != NULL) free(data);
+
+	cl_assert_(read_line(fp, true, true) == NULL,
+			   "Should get a NULL after we run out of file.");
+}
+
+void test_utils__read_line_by_line_from_multiline_without_nl(void) {
+	fp = fopen(CLAR_FIXTURE_PATH "multiline_text_no_nl", "r");
+
+	char *data = NULL;
+
+	data = read_line(fp, false, true);
+	cl_assert_equal_i(strcmp("I am a cat", data), 0);
+	if(data != NULL) free(data);
+
+	data = read_line(fp, true, true);
+	cl_assert_equal_i(strcmp("we are all cats", data), 0);
+	if(data != NULL) free(data);
+
+	cl_assert_(read_line(fp, true, true) == NULL,
+			   "Should get a NULL after we run out of file.");
 }
