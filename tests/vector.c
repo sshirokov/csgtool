96903c45245884e820f0c65d89655e3b0f45ed1b
Yaroslav Shirokov <slava@github.com>
2014-05-23 02:48:05 -0700

    uhhhhh

--- tests/vector.c
+++ tests/vector.c
@@ -9,6 +9,6 @@ void test_utils__distances(void) {
 	float d2 = f3_distance2(a, b);
 	float d =  f3_distance(a, b);
 
-	cl_assert_(d2 == 2, "d2 between origin and (1, 1) should be 1^2 + 1^2 = 2");
+	cl_assert_(d2 == 2.0, "d2 between origin and (1, 1) should be 1^2 + 1^2 = 2");
 	cl_assert_((d < 1.5) && (d > 1.4), "distance between origin and (1, 1) should be root(2)");
 }
