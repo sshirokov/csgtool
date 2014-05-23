bf2410f731982d42080d96e0f389f8f5fd466a7a
Yaroslav Shirokov <slava@github.com>
2014-05-23 02:44:39 -0700

    If this is what passes for code review, I'm embarrassed for the internet

--- tests/vector.c
+++ tests/vector.c
@@ -7,7 +7,7 @@ void test_utils__distances(void) {
 	float3 b = {1.0, 1.0, 0.0};
 
 	float d2 = f3_distance2(a, b);
-	float d =  f3_distance2(a, b);
+	float d =  f3_distance(a, b);
 
 	cl_assert_(d2 == 2, "d2 between origin and (1, 1) should be 1^2 + 1^2 = 2");
 	cl_assert_((d < 1.5) && (d > 1.4), "distance between origin and (1, 1) should be root(2)");
