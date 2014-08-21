f15618d223b221b024eac3f385ff1c194afa293a
Yaroslav Shirokov <slava@github.com>
2014-08-21 15:27:19 -0700

    Scaffold to add the `audit` command

---
+++ src/cmd_audit.c
@@ -0,0 +1,12 @@
+#include "cmd_audit.h"
+
+int cmd_audit(int argc, char *argv[]) {
+	// TOOD: Load mesh
+	// TODO: Walk polys
+
+	// TODO: Check:
+	//   * Zero length poly edges
+
+	// TODO: Release mesh
+	return 0;
+}
---
+++ src/cmd_audit.h
@@ -0,0 +1,9 @@
+#include "mesh.h"
+#include "poly.h"
+
+#ifndef __CMD_AUDIT_H
+#define __CMD_AUDIT_H
+
+int cmd_audit(int argc, char *argv[]);
+
+#endif
